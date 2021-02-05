23b90a5a66d6042f410b3b8fe3b0c46f8fb157b1
Isaac Sanchez <isanch624@west-mec.local>
2021-02-04 18:02:11 -0700

    finished prject

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglePopup(){
+    document.getElementById("Popup1").classList.toggle("active");
+}
+function togglePopup2(){
+    document.getElementById("Popup2").classList.toggle("active");
+}
