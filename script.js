d69e29343c47733a6e9dea6fa4d43a84e82651f5
Isaac Sanchez <isanch624@west-mec.local>
2021-02-05 18:17:08 -0700

    added an image

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function togglePopup(){
 function togglePopup2(){
     document.getElementById("Popup2").classList.toggle("active");
 }
+
+page.onclick =function(event){
+    let pageCoords = this.getBoundingClientRect();
+    console.log(page.clientTop)
+
+    let flagCoords = {
+        top: event.clientY - pageCoords.y - page.clientTop - flag.clientHeight/2,
+            left: event.clientX - pageCoords.x - page.clientLeft - flag.clientWidth/2
+    }
+    
+    if(flagCoords.top < 0) flagCoords.top = 0
+    if(flagCoords.left < 0) flagCoords.left = 0
+    if(flagCoords.left + flagCoords.clientWidth > page.clientWidth) flagCoords.left = page.clientWidth - flag.clientWidth;
+    if(flagCoords.left + flagCoords.clientHeight > page.clientWidth) flagCoords.top = page.clientHeight - flag.clientHeight;
+}
+
+flag.style.left = flagCoords.left + 'px'
+flag.style.top = flagCoords.top + 'px'
+
+
